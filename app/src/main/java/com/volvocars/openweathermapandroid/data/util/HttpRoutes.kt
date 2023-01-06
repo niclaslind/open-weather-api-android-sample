51b52c13cd0970afea66de2fb8f9fd53d777dddb
Niclas Lind <niclas.lind93@gmail.com>
2023-01-06 11:03:23 +0100

    Update HttpRoutes.kt

--- app/src/main/java/com/volvocars/openweathermapandroid/data/util/HttpRoutes.kt
+++ app/src/main/java/com/volvocars/openweathermapandroid/data/util/HttpRoutes.kt
@@ -3,5 +3,5 @@ package com.volvocars.openweathermapandroid.data.util
 object HttpRoutes {
     const val WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
     const val ICON_URL = "http://openweathermap.org/img/wn"
-    const val APP_ID = "a3b61c86047ddff06689c8fcabc467c1"
+    const val APP_ID = "YOUR_API_KEY_HERE"
 }
